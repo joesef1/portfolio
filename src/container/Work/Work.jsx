41288913f7d9d9f05e6568ad0ade64d52ef6af19
jousef1 <yousefel560@gmail.com>
2023-02-15 20:25:23 +0800

    work sass

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -19,7 +19,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap, MotionWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../Wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
